f79d81ce1b383378146d7f92fb6cac9d05bd8d27
rochdev <roch.devost@datadoghq.com>
2019-05-08 14:51:16 -0400

    wip

--- src/format.js
+++ src/format.js
@@ -23,6 +23,7 @@ function format (span) {
   extractError(formatted, span)
   extractTags(formatted, span)
   extractMetrics(formatted, span)
+  extractAnalytics(formatted, span)
 
   return formatted
 }
@@ -94,8 +95,6 @@ function extractError (trace, span) {
 function extractMetrics (trace, span) {
   const spanContext = span.context()
 
-  let analytics = spanContext._tags[ANALYTICS]
-
   Object.keys(spanContext._metrics).forEach(metric => {
     if (typeof spanContext._metrics[metric] === 'number') {
       trace.metrics[metric] = spanContext._metrics[metric]
@@ -105,18 +104,19 @@ function extractMetrics (trace, span) {
   if (spanContext._sampling.priority !== undefined) {
     trace.metrics[SAMPLING_PRIORITY_KEY] = spanContext._sampling.priority
   }
+}
 
-  switch (typeof analytics) {
-    case 'string':
-      analytics = parseFloat(analytics)
-    case 'number': // eslint-disable-line no-fallthrough
-      if (!isNaN(analytics)) {
-        trace.metrics[ANALYTICS_KEY] = Math.max(Math.min(analytics, 1), 0)
-      }
-      break
-    case 'boolean':
-      trace.metrics[ANALYTICS_KEY] = analytics ? 1 : 0
-      break
+function extractAnalytics (trace, span) {
+  let analytics = span.context()._tags[ANALYTICS]
+
+  if (analytics === true) {
+    analytics = 1
+  } else {
+    analytics = parseFloat(analytics)
+  }
+
+  if (!isNaN(analytics)) {
+    trace.metrics[ANALYTICS_KEY] = Math.max(Math.min(analytics, 1), 0)
   }
 }
 
--- test/analytics_sampler.spec.js
+++ test/analytics_sampler.spec.js
@@ -17,58 +17,23 @@ describe('analyticsSampler', () => {
   })
 
   describe('sample', () => {
-    it('should use a sample rate of 1 by default', () => {
-      sampler.sample(span, {
-        enabled: true
-      }, true)
+    it('should sample a span with the provided rate', () => {
+      sampler.sample(span, 0.5)
+
+      expect(span.setTag).to.have.been.calledWith(ANALYTICS, 0.5)
+    })
+
+    it('should not set a rate by default', () => {
+      sampler.sample(span, undefined, true)
+
+      expect(span.setTag).to.not.have.been.called
+    })
+
+    it('should inherit from global setting when unset', () => {
+      sampler.enable()
+      sampler.sample(span, undefined, true)
 
       expect(span.setTag).to.have.been.calledWith(ANALYTICS, 1)
-    })
-
-    it('should sample a span with the provided rate', () => {
-      sampler.sample(span, {
-        enabled: true,
-        sampleRate: 0.5
-      }, true)
-
-      expect(span.setTag).to.have.been.calledWith(ANALYTICS, 0.5)
-    })
-
-    it('should sample only when enabled', () => {
-      sampler.sample(span, {
-        sampleRate: 0.5
-      }, true)
-
-      expect(span.setTag).to.not.have.been.called
-    })
-
-    it('should sample only with the flag to use the default', () => {
-      sampler.sample(span, {
-        enabled: true,
-        sampleRate: 0.5
-      })
-
-      expect(span.setTag).to.not.have.been.called
-    })
-
-    it('should sample a span with the operation specific rate', () => {
-      sampler.sample(span, {
-        enabled: true,
-        sampleRates: {
-          'web.request': 0.5
-        }
-      })
-
-      expect(span.setTag).to.have.been.calledWith(ANALYTICS, 0.5)
-    })
-
-    it('should ignore invalid values', () => {
-      sampler.sample(span)
-      sampler.sample(span, 2)
-      sampler.sample(span, -1)
-      sampler.sample(span, 'foo')
-
-      expect(span.setTag).to.not.have.been.called
     })
 
     it('should ignore rates for different operation names', () => {
